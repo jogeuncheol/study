4f0d7e0b06db1b876913d6513536c650c9cd1804
jogeuncheol <blueqwerty@naver.com>
2023-04-14 14:00:29 +0900

    2023-04-14 lv1 second cpp

---
+++ lv1/2023_04_14_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo)
+{
+    vector<int> answer;
+
+    for (int i = 0; i < photo.size(); ++i)
+    {
+        answer.emplace_back(0);
+        for (int j = 0; j < photo[i].size(); ++j)
+        {
+            for (int k = 0; k < name.size(); ++k)
+            {
+                if (photo[i][j] == name[k])
+                    answer[i] += yearning[k];
+            }
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    for (int s : solution({"may", "kein", "kain", "radi"}, {5, 10, 1, 3}, {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}}))
+        cout << s << " ";
+    cout << endl;
+    return 0;
+}
