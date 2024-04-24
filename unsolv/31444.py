b4915b75de049280396e35e5e5f974d339b7e136
jogeuncheol <84510674+jogeuncheol@users.noreply.github.com>
2024-04-24 17:58:41 +0900

    Update 31444.py

--- unsolv/31444.py
+++ unsolv/31444.py
@@ -65,6 +65,70 @@ def solution(_n, _arr):
 	return answer
 
 
+# test
+# def transpose(_arr):
+#     arr_len = len(_arr[0])
+#     T_arr = [[0 for row in range(arr_len)] for col in range(arr_len)]
+
+#     for i in range(arr_len):
+#         for j in range(arr_len):
+#             T_arr[i][j] = _arr[j][i]
+#     return T_arr
+
+
+# def slicing(Sarr, start_idx, count):
+#     slice_arr = []
+
+#     for i in range(start_idx, count):
+#         slice_arr.append(Sarr[i][start_idx:count])
+#     return slice_arr
+
+
+# def swap_line(_arr, idx, n_idx):
+#     tmp = _arr.copy()
+#     _arr[idx] = _arr[n_idx]
+#     _arr[n_idx] = tmp[idx]
+
+#     T_arr = transpose(_arr)
+#     tmp = T_arr.copy()
+#     T_arr[idx] = T_arr[n_idx]
+#     T_arr[n_idx] = tmp[idx]
+#     return T_arr
+
+
+# def solution(_n, _arr):
+#     answer = 0
+
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             cal_arr = _arr.copy()
+#             swaped_arr = swap_line(cal_arr, i, j)
+
+#             for k in range(1, n - 1):
+#                 slice_arr = slicing(swaped_arr, 0, k + 1)
+#                 back_slice_arr = slicing(swaped_arr, k + 1, n)
+#                 flat_arr = sum(slice_arr, [])
+#                 back_flat_arr = sum(back_slice_arr, [])
+#                 flat_arr.sort()
+#                 back_flat_arr.sort()
+
+#                 x = 0
+#                 for l in flat_arr:
+#                     if l != x:
+#                         x = l
+#                         break
+#                 y = 0
+#                 if len(back_slice_arr[0]) > 1:
+#                     for l in back_flat_arr:
+#                         if l != y:
+#                             y = l
+#                             break
+#                 x = x if x < y else y
+#                 if x > answer:
+#                     answer = x
+#     return answer
+
+
 if __name__ == "__main__":
 	n = int(input())
 	arr = []
