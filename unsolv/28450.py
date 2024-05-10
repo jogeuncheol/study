998ecba9af4fc50d86b0dd8de5b54a59ce418f9e
jogeuncheol <84510674+jogeuncheol@users.noreply.github.com>
2024-05-10 16:15:54 +0900

    Add files via upload

--- unsolv/28450.py
+++ unsolv/28450.py
@@ -7,12 +7,6 @@ class ConventionalDead:
         self.H = 0
 
         self.solution_input()
-        self.test_map()
-
-    def test_map(self):
-        for i in range(10000000):
-            l = [0] * 1000
-            self.map.append(l)
 
     def solution_input(self):
         self.n, self.m = list(map(int, input().split(' ')))
@@ -27,20 +21,54 @@ class ConventionalDead:
             y, x = position
             self.answers.append(hh + self.map[y][x])
             return "YES"
+        elif 0 in self.answers:
+            return
+        # else:
+        #     y, x = position
+        #     if hp - self.map[y][x] < 0:
+        #         return
+        #     value = self.map[y][x]
+        #     self.map[y][x] = -1
+        #     right_value = self.map[y][x + 1] if x + 1 else -1
+        #     down_value = self.map[y + 1][x] if y + 1 else -1
+        #     if x + 1 < self.m:  # and self.map[y][x + 1] != -1:
+        #         if down_value != -1 and right_value < down_value:
+        #             self.f([y, x + 1], hp - value, hh + value)
+        #             if 0 in self.answers:
+        #                 return
+        #     if y + 1 < self.n:
+        #         if right_value >= down_value:
+        #             self.f([y + 1, x], hp - value, hh + value)
+        #             if 0 in self.answers:
+        #                 return
+        #     self.map[y][x] = value
+        # if False: return
         else:
             y, x = position
             if hp - self.map[y][x] < 0:
                 return
             value = self.map[y][x]
-            self.map[y][x] = -1
-            if x + 1 < self.m:  # and self.map[y][x + 1] != -1:
+            # self.map[y][x] = -1
+            right_value = self.map[y][x + 1] if x + 1 < self.m else -1
+            down_value = self.map[y + 1][x] if y + 1 < self.n else -1
+            if right_value != -1 and down_value != -1:
+                if right_value <= down_value:
+                    self.f([y, x + 1], hp - value, hh + value)
+                    if 0 in self.answers:
+                        return
+                if down_value <= right_value:
+                    self.f([y + 1, x], hp - value, hh + value)
+                    if 0 in self.answers:
+                        return
+            elif right_value != -1 and down_value == -1:
                 self.f([y, x + 1], hp - value, hh + value)
-            if y + 1 < self.n:
-                # if hp - self.map[y + 1][x] < 0:
-                #     self.map[y][x] = value
-                #     return
+                if 0 in self.answers:
+                    return
+            elif down_value != -1 and right_value == -1:
                 self.f([y + 1, x], hp - value, hh + value)
-            self.map[y][x] = value
+                if 0 in self.answers:
+                    return
+            # self.map[y][x] = value
 
     def solution(self):
         position = [0, 0]
